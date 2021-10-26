a215024755bc0e04e05f8d2f0b9a90bb72f0bf80
rou <you@example.com>
2021-10-26 14:14:46 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>2021年お勧めps4,ps5s</h1>
+<form action="search.php" method="post">
+    <!--任意の<input>要素＝入力欄などを用意する-->
+    <input type="text" name="search_name">
+    <!--送信ボタンを用意する-->
+    <input type="submit" name="submit" value="名前を検索する">
+</form>
---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<sidebar class="sidebar" id="sidebar">
+  <div class="searchbox">
+    <form  method="post" action="searchbox.php">
+      <input type="text"  name="word" value="" placeholder="word..." required>
+      <input type="submit" name="submit" value="検索" id="search" >
+      <label for="search"><i  class="fas fa-search"></i> </label>
+    </form>
+  </div>
+</sidebar>
